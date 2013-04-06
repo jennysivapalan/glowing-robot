1d8899dd95f83cf26bb3242e5ac7c7228e49c848
Jenny Sivapalan <jennifer.sivapalan@gmail.com>
2013-04-06 15:53:26 +0100

    Created case class from json

--- src/main/scala/com.proj/Dispatcher.scala
+++ src/main/scala/com.proj/Dispatcher.scala
@@ -1,8 +1,10 @@
 package com.proj
 
 import org.scalatra.ScalatraServlet
+import org.json4s._
 import org.json4s.native.JsonMethods._
 import org.json4s.DefaultFormats
+
 
 class Dispatcher extends ScalatraServlet {
 
@@ -12,12 +14,33 @@ class Dispatcher extends ScalatraServlet {
 
   get("/ons/:ons") {
     val ons_code = params("ons")
-    val source = scala.io.Source.fromFile("/Users/lindseydew/dev/hackdays/gov-hack-day/glowing-robot/src/main/scala/com.proj/cuts2012v2.json")
+    val source = scala.io.Source.fromFile("/home/administrator/dev/glowing-robot/src/main/scala/com.proj/cuts2012v2.json")
     val lines = source mkString
     implicit val formats = DefaultFormats
     val json = parse(lines)
 //    json.extract[Data]
-    json.values
+//   println(json \ "data")
+   val b =  json \ "data" match {
+        case JArray(x) => x map (y => y match {
+	 case JObject(z) => {
+          val borough = Borough(z(0)._2.extract[Double], 
+                  z(1)._2.extract[Double],
+                  z(2)._2.extract[String],
+                  z(3)._2.extract[String],
+                  z(4)._2.extract[String],
+                  z(5)._2.extract[String], 
+                  z(6)._2.extract[Double],
+                  z(7)._2.extract[Double],
+                  z(8)._2.extract[Double]
+
+)
+           
+           borough
+         }
+	})
+    }
+   println(b)
+   b 
   //  json \\ "ONS code"
 
   }
